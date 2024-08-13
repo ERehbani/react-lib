4e127ae844ae60627c3d8b83e438ca249469e67e
Elian Rehbani <elianrhbn@gmail.com>
2024-08-13 12:53:03 -0300

    delete useEffect

--- src/components/webcam/dropzone.tsx
+++ src/components/webcam/dropzone.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
 const MyDropzone = ({ cloudName, apiKey, presetName }) => {
   const [dataURL, setDataURL] = useState(null);
@@ -7,49 +7,49 @@ const MyDropzone = ({ cloudName, apiKey, presetName }) => {
   const dropzoneRef = useRef(null);
   const fileInputRef = useRef(null);
 
-  useEffect(() => {
-    const dropzone = dropzoneRef.current;
-    
-    const handleDragEnter = (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      setIsDragActive(true);
-    };
+  // useEffect(() => {
+  const dropzone = dropzoneRef.current;
 
-    const handleDragLeave = (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      setIsDragActive(false);
-    };
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragActive(true);
+  };
 
-    const handleDragOver = (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-    };
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragActive(false);
+  };
 
-    const handleDrop = (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      setIsDragActive(false);
-      
-      const files = e.dataTransfer.files;
-      if (files && files.length > 0) {
-        handleFiles(files);
-      }
-    };
+  const handleDragOver = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
 
-    dropzone.addEventListener('dragenter', handleDragEnter);
-    dropzone.addEventListener('dragleave', handleDragLeave);
-    dropzone.addEventListener('dragover', handleDragOver);
-    dropzone.addEventListener('drop', handleDrop);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragActive(false);
 
-    return () => {
-      dropzone.removeEventListener('dragenter', handleDragEnter);
-      dropzone.removeEventListener('dragleave', handleDragLeave);
-      dropzone.removeEventListener('dragover', handleDragOver);
-      dropzone.removeEventListener('drop', handleDrop);
-    };
-  }, []);
+    const files = e.dataTransfer.files;
+    if (files && files.length > 0) {
+      handleFiles(files);
+    }
+  };
+
+  //   dropzone.addEventListener('dragenter', handleDragEnter);
+  //   dropzone.addEventListener('dragleave', handleDragLeave);
+  //   dropzone.addEventListener('dragover', handleDragOver);
+  //   dropzone.addEventListener('drop', handleDrop);
+
+  //   return () => {
+  //     dropzone.removeEventListener('dragenter', handleDragEnter);
+  //     dropzone.removeEventListener('dragleave', handleDragLeave);
+  //     dropzone.removeEventListener('dragover', handleDragOver);
+  //     dropzone.removeEventListener('drop', handleDrop);
+  //   };
+  // }, []);
 
   const handleFiles = (files) => {
     const file = files[0];
@@ -82,7 +82,7 @@ const MyDropzone = ({ cloudName, apiKey, presetName }) => {
   const uploadImage = (e) => {
     e.stopPropagation(); // Previene la propagación del evento al contenedor
     const formData = new FormData();
-    const file = dataURLtoFile(dataURL, 'image.jpg');
+    const file = dataURLtoFile(dataURL, "image.jpg");
 
     formData.append("file", file);
     formData.append("upload_preset", presetName);
@@ -111,28 +111,31 @@ const MyDropzone = ({ cloudName, apiKey, presetName }) => {
   };
 
   const dataURLtoFile = (dataurl, filename) => {
-    let arr = dataurl.split(','),
-        mime = arr[0].match(/:(.*?);/)[1],
-        bstr = atob(arr[1]), 
-        n = bstr.length, 
-        u8arr = new Uint8Array(n);
-    while(n--){
-        u8arr[n] = bstr.charCodeAt(n);
+    let arr = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
     }
-    return new File([u8arr], filename, {type:mime});
+    return new File([u8arr], filename, { type: mime });
   };
 
   return (
     <div className="max-w-[300px] mx-auto mt-10">
-      <div 
-        ref={dropzoneRef}
+      <div
+        // ref={dropzoneRef}
         className="border border-dashed border-gray-500 rounded-md w-full flex justify-center items-center overflow-hidden"
         onClick={handleClick}
-      >
-        <input 
-          type="file" 
-          ref={fileInputRef} 
-          style={{display: 'none'}} 
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}>
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: "none" }}
           onChange={handleFileInputChange}
           accept="image/*"
         />
@@ -196,24 +199,22 @@ const MyDropzone = ({ cloudName, apiKey, presetName }) => {
               </button>
             </div>
           </div>
+        ) : isDragActive ? (
+          <div className="w-[298px] h-[140px] flex justify-center items-center transition-colors duration-300 bg-[#343333] text-[#545353]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              height="50"
+              width="50"
+              fill="currentColor">
+              <title>upload</title>
+              <path d="M1 14.5C1 12.1716 2.22429 10.1291 4.06426 8.9812C4.56469 5.044 7.92686 2 12 2C16.0731 2 19.4353 5.044 19.9357 8.9812C21.7757 10.1291 23 12.1716 23 14.5C23 17.9216 20.3562 20.7257 17 20.9811L7 21C3.64378 20.7257 1 17.9216 1 14.5ZM16.8483 18.9868C19.1817 18.8093 21 16.8561 21 14.5C21 12.927 20.1884 11.4962 18.8771 10.6781L18.0714 10.1754L17.9517 9.23338C17.5735 6.25803 15.0288 4 12 4C8.97116 4 6.42647 6.25803 6.0483 9.23338L5.92856 10.1754L5.12288 10.6781C3.81156 11.4962 3 12.927 3 14.5C3 16.8561 4.81833 18.8093 7.1517 18.9868L7.325 19H16.675L16.8483 18.9868ZM13 13V17H11V13H8L12 8L16 13H13Z" />
+            </svg>
+          </div>
         ) : (
-          isDragActive ? (
-            <div className="w-[298px] h-[140px] flex justify-center items-center transition-colors duration-300 bg-[#343333] text-[#545353]">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                height="50"
-                width="50"
-                fill="currentColor">
-                <title>upload</title>
-                <path d="M1 14.5C1 12.1716 2.22429 10.1291 4.06426 8.9812C4.56469 5.044 7.92686 2 12 2C16.0731 2 19.4353 5.044 19.9357 8.9812C21.7757 10.1291 23 12.1716 23 14.5C23 17.9216 20.3562 20.7257 17 20.9811L7 21C3.64378 20.7257 1 17.9216 1 14.5ZM16.8483 18.9868C19.1817 18.8093 21 16.8561 21 14.5C21 12.927 20.1884 11.4962 18.8771 10.6781L18.0714 10.1754L17.9517 9.23338C17.5735 6.25803 15.0288 4 12 4C8.97116 4 6.42647 6.25803 6.0483 9.23338L5.92856 10.1754L5.12288 10.6781C3.81156 11.4962 3 12.927 3 14.5C3 16.8561 4.81833 18.8093 7.1517 18.9868L7.325 19H16.675L16.8483 18.9868ZM13 13V17H11V13H8L12 8L16 13H13Z" />
-              </svg>
-            </div>
-          ) : (
-            <div className="w-full h-[140px] flex justify-center items-center text-center transition-colors duration-300">
-              Suelta tus archivos aquí o haz clic para seleccionar
-            </div>
-          )
+          <div className="w-full h-[140px] flex justify-center items-center text-center transition-colors duration-300">
+            Suelta tus archivos aquí o haz clic para seleccionar
+          </div>
         )}
       </div>
       {uploadedURL && (
